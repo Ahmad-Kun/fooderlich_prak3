49343b2436a9eadc2b70c2cb140256612596e768
Ahmad Al Farizi <a.farizi48@gmail.com>
2022-10-04 08:34:54 +0700

    New Commit

---
+++ lib/appDataProvider.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class AppDataProvider  extends InheritedWidget {
+  final AppData appData;
+  final Widget child;
+
+  const AppDataProvider({
+    Key? key,
+    required this.appData,
+    required this.child,
+  }) : super(key: key, child: child);
+
+  static AppDataProvider? of (BuildContext context) => 
+  context.dependOnInheritedWidgetOfExactType<AppDataProvider>();
+
+  @override
+  bool updateShouldNotify(covariant AppDataProvider oldWidget) {
+    return oldWidget != this ;
+  }
+}
+
+class AppData {
+  bool isFavorited;
+  AppData({required this.isFavorited});
+
+  changeFavorite () {
+     isFavorited = !isFavorited;
+  }
+}
