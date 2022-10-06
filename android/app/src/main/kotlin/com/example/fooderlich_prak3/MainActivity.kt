734b2dbf4655cbd82994aa5212720d7a9e3eec7c
Ahmad Al Farizi <a.farizi48@gmail.com>
2022-10-06 14:32:02 +0700

    Remove All

--- android/app/src/main/kotlin/com/example/fooderlich_prak3/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.fooderlich_prak3
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/config.dart
+++
@@ -1,5 +0,0 @@
-library config.globals;
-
-import 'my_theme.dart';
-
-MyTheme currentTheme = MyTheme();
