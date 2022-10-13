274f90f3db7e55058b0d1a663ae40117f5b08eff
Ahmad Al Farizi <a.farizi48@gmail.com>
2022-10-14 06:34:32 +0700

    Praktikum 6 - Final Draft

--- lib/home.dart
+++ lib/home.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
-import 'config.dart';
+import 'package:fooderlich/screens/recipes_screen.dart';
+import 'models/explore_recipe.dart';
 import 'screens/explore_screen.dart';
-import 'screens/recipes_screen.dart';
 import 'screens/grocery_screen.dart';
+import 'package:provider/provider.dart';
+import 'models/models.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -12,10 +14,11 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   int _selectedIndex = 0;
+  static bool isFavorited = false;
   static List<Widget> pages = <Widget>[
     ExploreScreen(),
     RecipesScreen(),
-    GroceryScreen(),
+    const GroceryScreen()
     // Card1(
     //     recipe: ExploreRecipe(
     //         authorName: 'Ray Wenderlich',
@@ -47,52 +50,38 @@ class _HomeState extends State<Home> {
     //           'Water'
     //         ],
     //         backgroundImage: 'assets/magazine_pics/mag3.png')),
-    
   ];
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
+  // void _onItemTapped(int index) {
+  //   setState(() {
+  //     _selectedIndex = index;
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'Fooderlich',
-            style: Theme.of(context).textTheme.headline6,
-          ),
-          actions: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                  child: IconButton(
-                    icon: const Icon(Icons.sunny),
-                    onPressed: () {
-                      currentTheme.switchTheme();
-                    },
-                  ),
-                ),
-              ],
-            )
-          ],
-        ),
-        body: pages[_selectedIndex],
-        bottomNavigationBar: BottomNavigationBar(
-            selectedItemColor:
-                Theme.of(context).textSelectionTheme.selectionColor,
-            currentIndex: _selectedIndex,
-            onTap: _onItemTapped,
-            items: <BottomNavigationBarItem>[
-              const BottomNavigationBarItem(
-                  icon: Icon(Icons.explore), label: 'Explore'),
-              const BottomNavigationBarItem(
-                  icon: Icon(Icons.book), label: 'Recipes'),
-              const BottomNavigationBarItem(
-                  icon: Icon(Icons.list), label: 'To Buy'),
-            ]));
+    return Consumer<TabManager>(
+      builder: (context, tabManager, child) {
+        return Scaffold(
+            appBar: AppBar(
+                title: Text('Fooderlich',
+                    style: Theme.of(context).textTheme.headline6)),
+            body: IndexedStack(index: tabManager.selectedTab, children: pages),
+            bottomNavigationBar: BottomNavigationBar(
+                selectedItemColor:
+                    Theme.of(context).textSelectionTheme.selectionColor,
+                currentIndex: tabManager.selectedTab,
+                onTap: (index) {
+                  tabManager.goToTab(index);
+                },
+                items: <BottomNavigationBarItem>[
+                  const BottomNavigationBarItem(
+                      icon: Icon(Icons.explore), label: 'Explore'),
+                  const BottomNavigationBarItem(
+                      icon: Icon(Icons.book), label: 'Recipes'),
+                  const BottomNavigationBarItem(
+                      icon: Icon(Icons.list), label: 'To Buy'),
+                ]));
+      },
+    );
   }
 }
